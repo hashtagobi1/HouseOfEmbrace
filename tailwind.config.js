c0668eff0f60a27b7dcc8e86e60bf626ad0ad17c
obi <anokwuruobi@gmail.com>
2022-07-09 22:29:54 +0100

    good shift, still needs work. did nav bar did search did footer did feat_art_link

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
       height: {
         "94": "22rem"
       },
+      // basicShadow: {
+      //   filter: "drop-shadow(5px 5px 0px #000)",
+      //   border: "10px solid red"
+      // },
+      fontFamily: {
+        hoe_Quest: ['Questrial', "sans-serif"],
+        hoe_Bodoni: ['Bodoni Moda', "serif"]
+      },
+      colors: {
+        HOE_MAIN: "#CEADDF",
+        HOE_MAIN_LIGHT: "#ebdef2",
+        HOE_GREEN_WARM: "#B9E1A8",
+        HOE_BACKGROUND: "#FFDCA8"
+      }
     },
   },
   plugins: [],
